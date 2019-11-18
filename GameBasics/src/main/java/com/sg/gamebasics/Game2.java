a23fec98038556c3435912f8a96d5ea182a86a4c
Ryan Horstman <ryanmhorstman@gmail.com>
2019-11-18 16:00:33 -0600

    changed game to include a moving ball

--- GameBasics/src/main/java/com/sg/gamebasics/Game2.java
+++ GameBasics/src/main/java/com/sg/gamebasics/Game2.java
@@ -8,6 +8,7 @@ package com.sg.gamebasics;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.geom.Ellipse2D;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -19,23 +20,35 @@ import javax.swing.JPanel;
 @SuppressWarnings("serial")
 public class Game2 extends JPanel {
     
-    public void paint(Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(Color.RED);
-        g2d.fillOval(0, 0, 30, 30);
-        g2d.drawOval(0, 50, 30, 30);
-        g2d.fillRect(50, 0, 30, 30);
-        g2d.drawRect(50, 50, 30, 30); 
-        
-        g2d.draw(new Ellipse2D.Double(0, 100, 30, 30));
+    Ball ball = new Ball(this);
+    
+    private void move() {
+        ball.move();
     }
     
-    public static void main(String[] args) {
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        ball.paint(g2d);
+    }
+    
+
+    public static void main(String[] args) throws InterruptedException {
         JFrame frame = new JFrame("Mini Tennis");
-        frame.add(new Game2());
-        frame.setSize(300, 300);
+        Game2 game = new Game2();
+        frame.add(game);
+        frame.setSize(300, 400);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        while (true) {
+            game.move();
+            game.repaint();
+            Thread.sleep(10);
+        }
         
     }
 }
