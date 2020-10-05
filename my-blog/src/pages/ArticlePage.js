b7680e0142baf3e059de04431cf8de43f5e0df02
Ryan Horstman <ryanmhorstman@gmail.com>
2020-10-05 13:58:17 -0500

    built backend for blog site

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , { useState, useEffect }from 'react'
 import ArticlesList from '../components/ArticlesList';
 import articles from './article-content';
 import articleContent from './article-content';
@@ -9,6 +9,12 @@ const ArticlePage = ({ match }) => {
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
 
+    useEffect(() => {
+        setArticleInfo({ upvotes: 3 });
+    }, [name]);
+
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: []});    
+
     if (!article) return <NotFoundPage />
 
     const otherArticles = articleContent.filter(article => article.name !== name);
@@ -17,6 +23,7 @@ const ArticlePage = ({ match }) => {
         <React.Fragment>
             <div>
                 <h1>{article.title}</h1>
+                <p>This post has been upvoted {articleInfo.upvotes} times</p>
                 {article.content.map((paragraph, key) => (
                     <p key={key}>{paragraph}</p>
                 ))}
